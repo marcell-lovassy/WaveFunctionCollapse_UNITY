66e99cdd0395b0ebf0f6db26ed27977d61ee5053
marcell-lovassy <marcell.lovassy@gmail.com>
2023-09-11 23:19:07 +0200

    It is working, but inner corners are not instantiated

--- Assets/Scripts/Procedural3D/MapCreator.cs
+++ Assets/Scripts/Procedural3D/MapCreator.cs
@@ -1,98 +1,208 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics.Tracing;
 using System.Linq;
+using Unity.Mathematics;
 using UnityEngine;
+
+public enum CellDirection
+{
+    Left, 
+    Right, 
+    Up, 
+    Down,
+    None
+}
+
+public class Cell
+{
+    public bool IsClosed = false;
+    public Vector2Int position;
+    public List<int> possibleKeys;
+
+    public Cell(int x, int y, IEnumerable<int> keys)
+    {
+        position = new Vector2Int(x, y);
+        possibleKeys = keys.ToList();
+    }
+
+    public CellDirection GetOthercCellRelativeDirection(Cell other)
+    {
+        if (other.position.x == position.x && other.position.y == position.y) return CellDirection.None;
+
+        if(other.position.x == position.x)
+        {
+            return other.position.y < position.y ? CellDirection.Down : CellDirection.Up;
+        }
+        else
+        {
+            return other.position.x < position.x ? CellDirection.Left : CellDirection.Right;
+        }
+    }
+}
 
 public class MapCreator : MonoBehaviour
 {
-    public MapDetailsSO mapDetails;
-    Dictionary<Vector3, TileAndRules> InstantiatedTiles = new Dictionary<Vector3, TileAndRules>();
-    int[,][] map;
+    [SerializeField]
+    private MapDetailsSO mapDetails;
+
+    List<Cell> closedCells = new List<Cell>();
+
+    Cell[,] map;
 
     private void Start()
     {
-        map = new int[mapDetails.maxMapTileCount, mapDetails.maxMapTileCount][];
+        map = new Cell[mapDetails.maxMapTileCount, mapDetails.maxMapTileCount];
         InitEmptyMap();
-        EvaluateInitialMap();
+        ExecuteWFC();
+
         StartCoroutine(CreateMap());
     }
+
     private void InitEmptyMap()
     {
-        for (int i = 0; i < mapDetails.maxMapTileCount; i++)
+        for (int x = 0; x < mapDetails.maxMapTileCount; x++)
         {
-            for (int j = 0; j < mapDetails.maxMapTileCount; j++)
+            for (int y = 0; y < mapDetails.maxMapTileCount; y++)
             {
-                if (i == 0 && j == 0)
+                //if(x == 0 && y == 0)
+                //{
+                //    map[x, y] = new Cell(x, y, new int[] { 5 });
+                //    continue;
+                //}
+                //TODO: get the keys from the mapDetails;
+                map[x, y] = new Cell(x, y, new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 });
+            }
+        }
+    }
+
+    private void ExecuteWFC()
+    {
+        Queue<Cell> openCells = new Queue<Cell>();
+
+        //select a random cell
+        var randomStartPosition = new Vector2Int(13, 13);///*GetRandomPositionOnMap();*/
+        var selectedCell = map[randomStartPosition.x, randomStartPosition.y];
+
+        openCells.Enqueue(selectedCell);
+        int safetyCounter = 0;
+        while (openCells.Count > 0 && safetyCounter < 900)
+        {
+            var cellToCollapse = openCells.Dequeue();
+            CollapseCell(cellToCollapse);
+            if(cellToCollapse.possibleKeys.FirstOrDefault() == -1)
+            {
+                continue;
+            }
+            List<Cell> neighbours = new List<Cell>();
+            PropagateChanges(cellToCollapse, ref neighbours);
+            foreach (var n in neighbours)
+            {
+                if (!openCells.Contains(n) && !n.IsClosed)
                 {
-                    map[i, j] = new int[] { 5 };
-                    continue;
-                }
-                map[i, j] = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8 };
-            }
-        }
-    }
-
-    private void EvaluateInitialMap()
-    {
-        UpdatePossibleTilesAtNeighbours(0, 0, map[0, 0]);
-    }
-
-    private void UpdatePossibleTilesAtNeighbours(int i, int j, int[] possibleTiles)
-    {
-        Vector2Int top = new Vector2Int (i, j + 1);
-        Vector2Int bottom = new Vector2Int(i, j - 1);
-        Vector2Int left = new Vector2Int(i - 1, j);
-        Vector2Int right = new Vector2Int(i + 1, j);
-
-        if(top.y < mapDetails.maxMapTileCount)
-        {
-            var possibleList = GetPossibleTopTiles(possibleTiles);
-            for (int k = 0; k < map[top.x, top.y].Length; k++)
-            {
-                if(!possibleList.Contains(map[top.x, top.y][k]))
-                {
-                    possibleList.Remove(map[top.x, top.y][k]);
+                    openCells.Enqueue(n);
                 }
             }
-            map[top.x, top.y] = possibleList.ToArray();
+            safetyCounter++;
         }
     }
 
-    private List<int> GetPossibleTopTiles(int[] possibleTiles)
+    private void CollapseCell(Cell cellToCollapse)
     {
-        List<int> possibleTilesList = new List<int>();
-
-        foreach (int tile in possibleTiles)
+        var randomCellType = UnityEngine.Random.Range(0, cellToCollapse.possibleKeys.Count);
+        if(cellToCollapse.possibleKeys.Count == 0)
         {
-            var rules = mapDetails.mapTiles.FirstOrDefault(t => t.key == tile);
-            foreach(var possibleKey in rules.keysOfValidNeighboursTop)
+            //mark the cell as empty
+            cellToCollapse.possibleKeys.Add(-1);
+            return;
+        }
+        var selectedType = cellToCollapse.possibleKeys[randomCellType];
+        cellToCollapse.possibleKeys.Clear();
+        cellToCollapse.possibleKeys.Add(selectedType);
+        cellToCollapse.IsClosed = true;
+        closedCells.Add(cellToCollapse);
+    }
+    
+    private List<Cell> PropagateChanges(Cell centerCell, ref List<Cell> neighbours)
+    {
+        var tileRules = mapDetails.mapTiles.FirstOrDefault(tile => tile.key == centerCell.possibleKeys[0]);
+        neighbours.AddRange(GetNeighbours(centerCell));
+        foreach (var neighbourCell in neighbours)
+        {
+            if(centerCell.GetOthercCellRelativeDirection(neighbourCell) == CellDirection.Left)
             {
-                possibleTilesList.Add(possibleKey);
+                //if (neighbourCell.possibleKeys.Count == 1) continue;
+                neighbourCell.possibleKeys = neighbourCell.possibleKeys.Intersect(tileRules.Left).ToList();
+            }
+            else if (centerCell.GetOthercCellRelativeDirection(neighbourCell) == CellDirection.Right)
+            {
+                //if (neighbourCell.possibleKeys.Count == 1) continue;
+                neighbourCell.possibleKeys = neighbourCell.possibleKeys.Intersect(tileRules.Right).ToList();
+            }
+            else if (centerCell.GetOthercCellRelativeDirection(neighbourCell) == CellDirection.Up)
+            {
+                //if (neighbourCell.possibleKeys.Count == 1) continue;
+                neighbourCell.possibleKeys = neighbourCell.possibleKeys.Intersect(tileRules.Up).ToList();
+            }
+            else if (centerCell.GetOthercCellRelativeDirection(neighbourCell) == CellDirection.Down)
+            {
+                //if (neighbourCell.possibleKeys.Count == 1) continue;
+                neighbourCell.possibleKeys = neighbourCell.possibleKeys.Intersect(tileRules.Down).ToList();
             }
         }
-
-        return possibleTilesList;
+        return neighbours.ToList();
     }
-
-  
 
     private IEnumerator CreateMap()
     {
-        for (int i = 0; i < mapDetails.maxMapTileCount; i++)
+        foreach (var cell in closedCells)
         {
-            for (int j = 0; j < mapDetails.maxMapTileCount; j++)
-            {
-                Instantiate(mapDetails.mapTiles.FirstOrDefault(t => t.key == map[i,j].FirstOrDefault()).tile, new Vector3(i, 0, j), Quaternion.identity);
-                yield return new WaitForSeconds(0.025f);
-            }
+            Instantiate(mapDetails.mapTiles.FirstOrDefault(t => t.key == cell.possibleKeys.FirstOrDefault()).tile, new Vector3(cell.position.x, 0, cell.position.y), Quaternion.identity);
+            yield return null;
         }
 
-        //var mapTiles = new List<TileAndRules>(mapDetails.mapTiles);
-        //foreach (var tile in mapTiles)
+        //for (int i = 0; i < mapDetails.maxMapTileCount; i++)
         //{
-        //    var tileInstance = Instantiate(tile.tile, Vector3.zero, Quaternion.identity);
-        //    InstantiatedTiles[tileInstance.transform.position] = tile;
+        //    for (int j = 0; j < mapDetails.maxMapTileCount; j++)
+        //    {
+        //        if (map[i, j].possibleKeys.FirstOrDefault() == -1) continue;
+        //        Instantiate(mapDetails.mapTiles.FirstOrDefault(t => t.key == map[i,j].possibleKeys.FirstOrDefault()).tile, new Vector3(i, 0, j), Quaternion.identity);
+        //        yield return null;//new WaitForSeconds(0.025f);
+        //    }
         //}
+    }
+
+    private List<Cell> GetNeighbours(Cell c)
+    {
+        List<Cell> neighbours = new List<Cell>();
+
+        if(c.position.x + 1 < mapDetails.maxMapTileCount) neighbours.Add(map[c.position.x + 1, c.position.y]);
+        if(c.position.x - 1 >= 0) neighbours.Add(map[c.position.x - 1, c.position.y]);
+        if(c.position.y + 1 < mapDetails.maxMapTileCount) neighbours.Add(map[c.position.x, c.position.y + 1]);
+        if(c.position.y - 1 >= 0) neighbours.Add(map[c.position.x, c.position.y - 1]);
+
+        return neighbours;
+    }
+
+    private Vector2Int GetRandomPositionOnMap()
+    {
+        return new Vector2Int(UnityEngine.Random.Range(0, mapDetails.maxMapTileCount), UnityEngine.Random.Range(0, mapDetails.maxMapTileCount));
+    }
+
+    private Cell GetLowestEntropyCell()
+    {
+        int lowestEntropy = 9;
+        Cell nextCell = null;
+        foreach (var cell in map)
+        {
+            if(cell.possibleKeys.Count != 1 && cell.possibleKeys.Count < lowestEntropy)
+            {
+                lowestEntropy = cell.possibleKeys.Count;
+                nextCell = cell;
+            }
+        }
+        return nextCell;    
     }
 }
--- Assets/Scripts/Procedural3D/MapDetailsSO.cs
+++ Assets/Scripts/Procedural3D/MapDetailsSO.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "Map_", menuName = "MapGenerator/Create Map Details")]
@@ -15,17 +16,88 @@ public class MapDetailsSO : ScriptableObject
 [Serializable]
 public class TileAndRules
 {
+    public List<int> Left 
+    {
+        get
+        {
+            if (leftList != null && leftList.Count > 0) return leftList;
+            leftList = new List<int>();
+            if (!string.IsNullOrEmpty(left))
+            {
+                var splittedString = left.Split(',').ToList();
+                foreach (var key in splittedString)
+                {
+                    leftList.Add(int.Parse(key));
+                }
+            }
+            return leftList;
+        }
+    }
+
+    public List<int> Right
+    {
+        get
+        {
+            if (rightList != null && rightList.Count > 0) return rightList;
+            rightList = new List<int>();
+            if (!string.IsNullOrEmpty(right))
+            {
+                var splittedString = right.Split(',').ToList();
+                foreach (var key in splittedString)
+                {
+                    rightList.Add(int.Parse(key));
+                }
+            }
+            return rightList;
+        }
+    }
+
+    public List<int> Up
+    {
+        get
+        {
+            if (upList != null && upList.Count > 0) return upList;
+            upList = new List<int>();
+            if (!string.IsNullOrEmpty(up))
+            {
+                var splittedString = up.Split(',').ToList();
+                foreach (var key in splittedString)
+                {
+                    upList.Add(int.Parse(key));
+                }
+            }
+            return upList;
+        }
+    }
+
+    public List<int> Down
+    {
+        get
+        {
+            if (downList != null && downList.Count > 0) return downList;
+            downList = new List<int>();
+
+            if (!string.IsNullOrEmpty(down))
+            {
+                var splittedString = down.Split(',').ToList();
+                foreach (var key in splittedString)
+                {
+                    downList.Add(int.Parse(key));
+                }
+            }
+            return downList;
+        }
+    }
+
     public int key;
     public GameObject tile;
-    public int[] keysOfValidNeighboursLeft;
-    public int[] keysOfValidNeighboursRight;
-    public int[] keysOfValidNeighboursBottom;
-    public int[] keysOfValidNeighboursTop;
-}
+    public string left;
+    public string right;
+    public string down;
+    public string up;
 
-[Serializable]
-public class TileData
-{
-    public GameObject tile;
-    public float probability;
+    private List<int> leftList;
+    private List<int> rightList;
+    private List<int> upList;
+    private List<int> downList;
 }
